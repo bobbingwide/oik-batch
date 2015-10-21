d0499a9af441d55f1ab64ba7cef401488e5099cb
Herb Miller <herb@bobbingwide.com>
2015-10-21 10:56:02 +0100

    Update ignore lists
    - _ca_checkignorelist should no longer be used. Deprecated by use of gobang();!
    - Added some @TODOs
    - Added buddypress bp-forums/bbpress to ignore directories after discussion with Paul Gibbs about it being deprecated code
    - Added .git to ignore directories
    - Removed oik-activation from ignore files - decided that this file should be documented for each plugin that uses it
    - Spelling correction from Passing to Parsing

--- oik-ignore-list.php
+++ oik-ignore-list.php
@@ -1,4 +1,4 @@
-<?php // (C) Copyright Bobbing Wide 2013, 2014
+<?php // (C) Copyright Bobbing Wide 2013-2015
 
 /**
  * Add files to ignore when the plugin is NOT "wordpress"
@@ -6,6 +6,7 @@
  * These are mostly being ignored since they contained OO code or 
  * LOTS of functions we really didn't care much about.
  *
+ * @TODO Revisit since a) We can handle OO code now b) We might actually care about some of them
  *
  * 
  */
@@ -26,19 +27,20 @@ function _ca_not_wordpress_ignore_list() {
  * 
  * @TODO Change so that po, mo and other files are NOT ignored for the "wordpress" plugin
  * 
- *
+ * NOTE THE GOBANG!
  */
+if ( !function_exists( "_ca_checkignorelist" ) ) { 
 function _ca_checkignorelist( $file ) {
   gobang();
   $dir = pathinfo( $file, PATHINFO_DIRNAME );
-  $ignore_dirs = bw_assoc( bw_as_array( "oik-tunes/getid3," ) );
+  $ignore_dirs = bw_assoc( bw_as_array( "oik-tunes/getid3,buddypress/bp-forums/bbpress" ) );
   $ignore = bw_array_get( $ignore_dirs, $dir, false );
   if ( $ignore ) {
     // echo "Ignoring folder: $dir ". PHP_EOL;
   } else {  
     $filename = pathinfo( $file, PATHINFO_FILENAME );
     // $ignores = bw_assoc( bw_as_array( "oik-activation,oik-docblock,bobbnotwp" ) );
-    $ignore_files = "wp-config,oik-activation,oik-docblock,bobbnotwp";
+    $ignore_files = "wp-config,oik-docblock,bobbnotwp";
     //if ( $plugin !== "wordpress" ) {
       $ignore_files .= _ca_not_wordpress_ignore_list(); 
     //}
@@ -47,11 +49,29 @@ function _ca_checkignorelist( $file ) {
     if ( $ignore ) {
       echo "Ignoring file: $filename " . PHP_EOL;
     } else { 
-      echo "Passing file: $filename " . PHP_EOL ;
+      echo "Parsing file: $filename " . PHP_EOL ;
       //goban();
     }
   }
   return( $ignore );
+}
+}
+
+/**
+ * 
+ */
+function _la_ignore_dir( $ignore_dirs, $dir ) {
+	$ignore = false;
+	foreach ( $ignore_dirs as $idir ) {
+		if ( strpos( $dir, $idir ) === 0 ) {
+			$ignore = true;
+			break;
+		} else {
+			//echo $dir . PHP_EOL;
+			//echo $idir . PHP_EOL;
+		}
+	}
+	return( $ignore );
 }
 
 
@@ -64,26 +84,41 @@ function _ca_checkignorelist( $file ) {
  * 
  */  
 function _la_checkignorelist( $file ) {
-  $dir = pathinfo( $file, PATHINFO_DIRNAME );
-  $ignore_dirs = bw_assoc( bw_as_array( "oik-tunes/getid3," ) );
-  $ignore = bw_array_get( $ignore_dirs, $dir, false );
-  static $ignored = 0;
-  if ( $ignore ) {
-    // echo "Ignoring folder: $dir ". PHP_EOL;
-  } else {  
-    $filename = pathinfo( $file, PATHINFO_FILENAME );
-    // $ignores = bw_assoc( bw_as_array( "oik-activation,oik-docblock,bobbnotwp" ) );
-    $ignore_files = "wp-config,oik-activation,oik-docblock,bobbnotwp,geshi,cron-svn-pots,extract,ExtractTest,l10n,makepot,mo,po,streams,pot-ext-meta,";
-    $ignore_files .= ",extension.cache.dbm,extension.cache.mysql,getid3.lib,module.archive.gzip,module.archive.rar";
-    $ignores = bw_assoc( bw_as_array( $ignore_files ) );
-    $ignore = bw_array_get( $ignores, $filename, false );
-    if ( $ignore ) {
-      $ignored++;
-      echo "Ignoring $ignored file: $filename " . PHP_EOL;
-    } else { 
-      echo "Passing file: $filename " . PHP_EOL ;
-      //goban();
-    }
-  }
-  return( $ignore );
+	static $ignoring = null;
+	static $ignored = 0;
+	$dir = pathinfo( $file, PATHINFO_DIRNAME );
+	//echo $file . PHP_EOL;
+	//echo $dir . PHP_EOL;
+	//echo $ignoring . PHP_EOL;
+	if ( $ignoring && ( 0 === strpos( $dir, $ignoring ) ) ) {
+		$ignore = true;
+	} else {
+		$ignore_dirs = bw_assoc( bw_as_array( "getid3,bp-forums/bbpress,languages,.git" ) );
+		//$ignore = bw_array_get( $ignore_dirs, $dir, false );
+		$ignore = _la_ignore_dir( $ignore_dirs, $dir );
+		if ( $ignore ) {
+			$ignoring = $dir;
+			//gob();
+		} else {
+			$ignoring = null;
+		}
+	}
+	if ( $ignore ) {
+		echo "Ignoring folder: $dir ". PHP_EOL;
+	} else {	
+		$filename = pathinfo( $file, PATHINFO_FILENAME );
+		// $ignores = bw_assoc( bw_as_array( "oik-activation,oik-docblock,bobbnotwp" ) );
+		$ignore_files = "wp-config,oik-docblock,bobbnotwp,geshi,cron-svn-pots,extract,ExtractTest,l10n,makepot,mo,po,streams,pot-ext-meta,";
+		$ignore_files .= ",extension.cache.dbm,extension.cache.mysql,getid3.lib,module.archive.gzip,module.archive.rar";
+		$ignores = bw_assoc( bw_as_array( $ignore_files ) );
+		$ignore = bw_array_get( $ignores, $filename, false );
+		if ( $ignore ) {
+			$ignored++;
+			echo "Ignoring $ignored file: $filename " . PHP_EOL;
+		} else { 
+			echo "Parsing file: $filename " . PHP_EOL ;
+			//goban();
+		}
+	}
+	return( $ignore );
 }

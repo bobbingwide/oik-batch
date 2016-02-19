16562a9b4f4826039ca3349661c8b51380047710
Herb Miller <herb@bobbingwide.com>
2016-02-19 13:09:43 +0000

    Issue #2 - Support direct invocation of batch logic in a plugin

--- libs/oik-cli.php
+++ libs/oik-cli.php
@@ -16,16 +16,51 @@
  *
  * This routine does not make any decisions.
  * If you want to stop you just press Ctrl-Break.
+ *
  */
 if ( !function_exists( 'docontinue' ) ) { 
-function docontinue( $plugin ) {
-   echo PHP_EOL;
-    echo "Continue? $plugin ";
-    $stdin = fopen( "php://stdin", "r" );
-    $response = fgets( $stdin );
-		$response = trim( $response );
-    fclose( $stdin );
-    return( $response );
+function docontinue( $plugin="Press Ctrl-Break to halt" ) {
+	echo PHP_EOL;
+	echo "Continue? $plugin ";
+	$stdin = fopen( "php://stdin", "r" );
+	$response = fgets( $stdin );
+	$response = trim( $response );
+	fclose( $stdin );
+	return( $response );
+}
+}
+
+if ( !function_exists( "oik_batch_run_me" ) ) {
+function oik_batch_run_me( $me ) {
+	$run_me = false;
+	echo PHP_SAPI;
+	echo PHP_EOL;
+	$included_files = get_included_files();
+	// print_r( $included_files[0] );
+	if ( $included_files[0] == __FILE__) {
+		$run_me = true;
+	} else {
+		//  has been loaded by another PHP routine so that routine is in charge. e.g. boot-fs.php for WP-CLI
+		$basename = basename( $included_files[0] );
+		if ( $basename == "oik-wp.php" ) {
+			print_r( $_SERVER );
+			$fetched = bw_array_get( $_SERVER['argv'], 1, null );
+			if ( $fetched ) {
+				$fetched_basename = basename( $fetched );
+				$me_basename = basename( $me );
+				$run_me = ( $fetched_basename == $me_basename );
+			}	
+		}
+		if ( $basename == "oik-batch.php" ) {
+		
+			print_r( $_SERVER );
+			$fetched = $_SERVER['argv'][0];
+			$fetched_basename = basename( $fetched );
+			$me_basename = basename( $me );
+			$run_me = ( $fetched_basename == $me_basename );
+		}
+	}
+	return( $run_me );
 }
 }
 

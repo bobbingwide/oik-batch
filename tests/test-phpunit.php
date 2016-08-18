13c44c3836ac6cdd8c41b435950c24ddd075cd99
Herb Miller <herb@bobbingwide.com>
2016-08-18 14:43:21 +0100

    First pass adding support for PHPUnit

---
+++ tests/test-phpunit.php
@@ -0,0 +1,19 @@
+<?php // (C) Copyright Bobbing Wide 2016 
+
+//use PHPUnit\Framework\TestCase;
+	
+echo "There you go: " . __FILE__ . PHP_EOL;
+	
+class UnitTest extends PHPUnit_Framework_TestCase  {
+	
+	public function test_case() {
+		$this->assertTrue( true );
+	}
+		
+		
+	public function test_oik_path_available() {
+		$this->assertTrue( function_exists( 'oik_path' ) );
+	}
+
+}
+	

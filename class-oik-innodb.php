b1187e964ff856d222cc76fe98cde7d116a752ef
Herb Miller <herb@bobbingwide.com>
2016-12-22 16:59:05 +0000

    Issue #17 - logic to detect MyISAM tables and convert to InnoDB

---
+++ class-oik-innodb.php
@@ -0,0 +1,95 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+class OIK_innodb {
+
+	public $results;
+	public $counts;
+
+	/**
+	 * Constructor for OIK_innodb
+	 */
+	function __construct() {
+		$this->reset();
+	}
+	
+	/**
+	 * Load and count the tables by engine
+	 */
+	function reset() {
+		$this->query_table_engines();
+		$this->count_tables();
+	}
+	
+	/**
+	 * Convert any MyISAM tables to InnoDB
+	 */
+	function maybe_convert() {
+		$myisam_table_count = $this->query_count( "MyISAM" );
+		if ( $myisam_table_count ) {
+			$this->convert_engine();
+			$this->reset();
+		}	
+	}
+
+	/**
+	 * Query the engine for each table in the table_schema
+	 *
+	 * 
+	 * select table_name, engine from information_schema.tables where table_schema = 'herb_wp373';
+	 */
+	function query_table_engines() {
+		global $wpdb;
+		$query = $wpdb->prepare( "select table_name, engine from information_schema.tables where table_schema = %s", DB_NAME );
+		bw_trace2( $query, "query" );
+		$this->results = $wpdb->get_results( $query );
+		return( $this->results );
+	}
+
+	/**
+	 * Count the different engines for the tables in the database
+	 *
+	 */
+	function count_tables() {	
+		$this->counts = array();
+		foreach ( $this->results as $result ) {
+			if ( !isset( $this->counts[ $result->engine] ) ) {
+				$this->counts[ $result->engine ] = 0;
+			}
+			$this->counts[ $result->engine ] += 1;
+		}
+		bw_trace2( $this->counts, "Counts for: " . DB_NAME );
+	}
+	
+	/**
+	 * Return the number of tables for the engine
+	 * 
+	 * @param string $engine InnoDB, MyISAM, etc
+	 * @return integer count
+	 */ 
+	function query_count( $engine="MyISAM" ) {
+		$count = bw_array_get( $this->counts, $engine, 0 );
+		return( $count );
+	}
+
+	/**
+	 * Convert tables from one engine to another
+	 *
+	 * We assume that the WordPress user has the authority to alter tables
+	 *
+	 * 
+	 * @param string $from_engine 
+	 * @param string $to_engine
+	 * 
+	 */
+	function convert_engine( $from_engine="MyISAM", $to_engine="InnoDB" ) {
+		global $wpdb;
+		foreach ( $results as $result ) {
+			if ( $from_engine == $result->engine ) {
+				$query = $wpdb->prepare( "ALTER TABLE %s engine=%s", DB_NAME . "." . $result->table_name, $to_engine );
+				echo $query;
+				$rows_affected = $wpdb->query( $query );
+			}
+		}
+	}
+	
+}	
---
+++ oik-innodb.php
@@ -0,0 +1,30 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+if ( PHP_SAPI !== "cli" ) { 
+	die();
+}
+
+oik_innodb_loaded();
+
+/**
+ * Ensure all the tables in the database have an engine type of InnoDB rather than MyISAM
+ *
+ * The conversion is only performed if invoked as 
+ * oikwp oik-innodb.php convert=y
+ */
+function oik_innodb_loaded() {
+	oik_require( "class-oik-innodb.php", "oik-batch" );
+	$oik_innodb = new OIK_innodb();
+	$myisam_count = $oik_innodb->query_count();
+	
+	echo "MyISAM tables: " . $myisam_count . PHP_EOL;
+	if ( $myisam_count ) {
+		$convert = oik_batch_query_value_from_argv( "convert", null );
+		$convert = bw_validate_torf( $convert );
+		if ( $convert ) {
+			echo "Converting the MyISAM tables to InnoDB" . PHP_EOL;
+			$this->maybe_convert();
+		}	
+	} 
+}		 
+
